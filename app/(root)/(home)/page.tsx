893621679c9bb2b6fb56e0629451f819a9328e8f
ali <aliismail30sep@gmail.com>
2024-04-16 21:16:55 +0500

    change

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Home = () => {
 	return (
 		<section className=' flex size-full flex-col gap-10 text-white'>
-			<h1 className='text-4xl font-bold font-sedan'>Home</h1>
+			<h1 className='text-4xl font-bold font-sedan'>NEw HOme</h1>
 		</section>
 	);
 };
