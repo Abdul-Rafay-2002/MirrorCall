f8a727b288242d1130697cddc0de0aaac241101a
Rafay Dev <rafaymalick68@gmail.com>
2024-04-19 22:44:10 +0500

    new changes

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -13,17 +13,26 @@ const Home = async ()  => {
 
 return (
 	<section className=' flex size-full flex-col gap-8 text-white'>
-			<div className={`${hour >= "21" ? 'bg-night' : (hour > "12" ? 'bg-afternoon' : 'bg-day')} h-[303px] flex flex-col justify-between  items-start rounded-xl w-full px-10 py-9`}>
-			<div className='flex justify-between size-full  '>
-				<div className='z-10 relative flex justify-between flex-col'>
-					<h1 className= 'greet text-white text-6xl font-bold'>
-					{ ( hour >= "21" ? "Good Night" : (hour > "12" ? "Good Afternoon" : "Good Day"))}
+		<div
+			className={`${
+				hour >= '21' ? 'bg-night' : hour > '12' ? 'bg-afternoon' : 'bg-day'
+			} h-[303px] flex flex-col justify-between  items-start rounded-xl w-full px-10 py-9`}>
+			<div className='flex justify-between size-full flex-row-reverse '>
+				<div className='z-10 relative flex justify-end flex-col'>
+					<h1 className='greet text-white text-6xl font-bold font-anek-devanagari'>
+						{hour >= '21'
+							? '🌙 Good Night'
+							: hour > '12'
+							? '🌄 Good Afternoon'
+							: '☀️ Good Day'}
 					</h1>
-					<p className='text-2xl capitalize font-semibold '>{user?.firstName + " " + user?.lastName}</p>
+					<p className='text-4xl capitalize font-semibold font-anek-devanagari text-right mt-3 text-white/60'>
+						{user?.firstName + ' ' + user?.lastName}
+					</p>
 				</div>
-				
+
 				<div className=' z-10 relative flex justify-end flex-col'>
-				<p className='text-[#ECF0FF] bg-slate-950/40 px-4 py-2  rounded-lg text-base mb-20'>
+					<p className='text-[#ECF0FF] bg-slate-950/40 px-4 py-2  rounded-lg text-base mb-20'>
 						Upcoming Meeting is at: 12:30 PM
 					</p>
 					<p className='timezone text-[#ECF0FF] text-[65px] leading-[72px] font-extrabold pb-3'>
@@ -34,11 +43,11 @@ return (
 					</p>
 				</div>
 			</div>
-			</div>
+		</div>
 
-			<MeetingTypeList/>
-		</section>
-	);
+		<MeetingTypeList />
+	</section>
+);
 };
 
 export default Home;
